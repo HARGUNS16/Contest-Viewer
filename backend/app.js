c46fbbca58be969298408a168213b30372cc16fd
Hargun Singh <hargunstar16@gmail.com>
2022-09-27 19:49:35 +0530

    First Commit

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const schedule = require("node-schedule");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const express = require("express");
+const UpdateContestData = require("./controller/FetchContestData");
+const contestRouter = require("./router/contestRouter");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+//Starting the Server
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Started!!");
+});
+
+//Connecting Database
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB)
+  .then((res) => {
+    console.log("connection Succesful");
+  })
+  .catch((e) => {
+    console.log("error");
+  });
+
+app.use("/api/v1/contestData", contestRouter);
+
+// Updates Contests Data every Day
+schedule.scheduleJob("*/10 * * * *", UpdateContestData);
+
+//Server Error
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
+});
---
+++ frontend/src/pages/ContestPage.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import LoadingScreen from "../utils/LoadingScreen";
+import axios from "axios";
+import Error from "../utils/Error";
+import ContestList from "../components/ContestList";
+import Filter from "../components/Filter";
+import Slider from "../components/Slider";
+
+const filterData = (data) => {
+  let filetedData = [];
+  data.forEach((element) => {
+    const data1 = new Date(element.ContestDate);
+    const curData = new Date();
+    if (data1 >= curData) {
+      filetedData.push(element);
+    }
+  });
+  return filetedData;
+};
+
+const ContestPage = () => {
+  const [contestDetails, contestDetailsHandler] = useState([]);
+  const [isLoading, loaderHandler] = useState(false);
+  const [error, ErrorHandler] = useState(null);
+  const [filteredList, changeFilteredList] = useState([]);
+
+  const changeContestList = (platform) => {
+    if (platform === "All Contest") {
+      changeFilteredList(contestDetails);
+    } else {
+      let list = [];
+      contestDetails.forEach((element) => {
+        if (element.ContestPlatform === platform) list.push(element);
+      });
+      changeFilteredList(list);
+    }
+  };
+
+  //Fetching Contest Details
+
+  useEffect(() => {
+    async function fetchText() {
+      loaderHandler(true);
+      const data = await axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_BACKEND_URL}/contestData`,
+      });
+      const filteredData = filterData(data.data.data.contests);
+      loaderHandler(false);
+      contestDetailsHandler(filteredData);
+      changeFilteredList(filteredData);
+    }
+    fetchText().catch((error) => {
+      loaderHandler(false);
+      ErrorHandler(error.message);
+    });
+  }, []);
+
+  return (
+    <>
+      {isLoading && <LoadingScreen />}
+      {!isLoading && error && <Error error={error} />}
+      {!isLoading && !error && <Slider />}
+      {!isLoading && !error && <Filter changeContestList={changeContestList} />}
+      {!isLoading && !error && <ContestList contests={filteredList} />}
+    </>
+  );
+};
+
+export default ContestPage;
